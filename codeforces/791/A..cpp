d2c0d735e2700644ff93be6685374eed1018f043
Asad <mdasad.binsaber@gmail.com>
2020-04-09 20:35:00 +0600

    Add solution for problem `A - Bear and Big Brother` Link: https://codeforces.com/contest/791/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/791/submission/76002080

---
+++ codeforces/791/A..cpp
@@ -0,0 +1,41 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 09 04 2020
+
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+#define    ll   long long
+
+void task()
+{
+    ll a, b, i=0;
+
+    cin >> a >> b;
+
+    while(1){
+        i++;
+        a *= 3;
+        b *= 2;
+        if(a>b){
+            cout<<i<<endl;
+            return;
+        }
+    }
+}
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
