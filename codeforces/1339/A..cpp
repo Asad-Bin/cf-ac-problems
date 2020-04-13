9d1ffe9c9a1bfc66460bb0b4b5abbd11378f88eb
Asad <mdasad.binsaber@gmail.com>
2020-04-13 06:26:00 +0600

    Add solution for problem `A - Filling Diamonds` Link: https://codeforces.com/contest/1339/problem/A Tags: brute force, dp, implementation, math, *900 Ref: https://codeforces.com/contest/1339/submission/76438064

---
+++ codeforces/1339/A..cpp
@@ -0,0 +1,45 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 12 04 2020
+
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+#define    ll   long long
+
+void task()
+{
+    ll t, n;
+
+    cin >> t;
+
+    while(t--){
+        cin >> n;
+
+        cout << n << endl;
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    //clock_t tStart = clock();
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    //printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    return 0;
+}
