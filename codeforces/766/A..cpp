d9092ea6060b2516f721a2e9aec5764aec1a9357
Asad <mdasad.binsaber@gmail.com>
2020-05-05 20:47:00 +0600

    Add solution for problem `A - Mahmoud and Longest Uncommon Subsequence` Link: https://codeforces.com/contest/766/problem/A Tags: constructive algorithms, strings, *1000 Ref: https://codeforces.com/contest/766/submission/79066889

---
+++ codeforces/766/A..cpp
@@ -0,0 +1,60 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 05 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+void task()
+{
+    string a, b;
+
+    cin >> a >> b;
+    int len_a = a.length(), len_b = b.length();
+    if(len_a<len_b){
+        swap(a, b);
+        swap(len_a, len_b);
+    }
+
+    if(a==b) cout << "-1\n";
+    else cout << len_a << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
