015b35a4fb460f8f752520416b3b3af06260e9aa
Asad <mdasad.binsaber@gmail.com>
2020-06-09 18:13:00 +0600

    Add solution for problem `D - Solve The Maze` Link: https://codeforces.com/contest/1365/problem/D Tags: constructive algorithms, dfs and similar, dsu, graphs, greedy, implementation, shortest paths, *1700 Ref: https://codeforces.com/contest/1365/submission/83217268

--- codeforces/1365/D..cpp
+++ codeforces/1365/D..cpp
@@ -50,34 +50,109 @@ ll comb(ll a, ll b)
 }
 
 string s[50];
-bool vis[50][50];
-
-bool bfs(int n, int m)
+bool sbfs(pii source, int n, int m)
 {
     queue<pii> Q;
-    pii u;
+    pii u, v;
     int K, L;
-    if(s[n][m]!='#') Q.push(MP(n, m));
+    Q.push(source);
 
     while(!Q.empty()){
         u = Q.front();
         Q.pop();
         K = u.first;
         L = u.second;
+        if(K==n && L==m) return 1;
+
+        if(L>0 && (s[K][L-1]=='.' || s[K][L-1]=='G')){
+            if(K==n && L-1==m) return 1;
+            Q.push(MP(K, L-1)); //cout << "hi\n";
+        }
+        if(K<n && (s[K+1][L]=='.' || s[K+1][L]=='G')){
+            if(K+1==n && L==m) return 1;
+            Q.push(MP(K+1, L));
+        }
+        if(L<m && (s[K][L+1]=='.' || s[K][L+1]=='G')){
+            if(K==n && L+1==m) return 1;
+            Q.push(MP(K, L+1));
+        }
+        if(K>0 && (s[K-1][L]=='.' || s[K-1][L]=='G')){
+            if(K-1==n && L==m) return 1;
+            Q.push(MP(K-1, L));
+        }
+    }
+
+    return 0;
+}
+
+bool vis[50][50];
+
+bool bfs(int n, int m)
+{
+    //for(int K=0; K<=n; K++) cout << s[K] << "\n";
+    queue<pii> Q;
+    pii u;
+    int K, L;
+    if(s[n][m]!='#') Q.push(MP(n, m));
+
+    while(!Q.empty()){
+        //cout << "hi\n";
+        u = Q.front();
+        Q.pop();
+        K = u.first;
+        L = u.second;
         if(vis[K][L]) continue;
         vis[K][L] = 1;
+        //cout << K << ' ' << L << "\n";
+        //if(K==n && L==m) return 1;
 
-        if(L>0 && !vis[K][L-1] && (s[K][L-1]=='.' || s[K][L-1]=='G')) Q.push(MP(K, L-1));
-        else if(L>0 && !vis[K][L-1] && s[K][L-1]=='B') return 0;
+        //if(L>0 && vis[K][L-1]) continue;
+        if(L>0 && !vis[K][L-1] && (s[K][L-1]=='.' || s[K][L-1]=='G')){
+            //if(K==n && L-1==m) return 1;
+            //vis[K][L-1] = 1;
+            Q.push(MP(K, L-1)); //cout << "hi\n";
+        }
+        else if(L>0 && !vis[K][L-1] && s[K][L-1]=='B'){
+            //if(K==n && L-1==m) return 1;
+            //Q.push(MP(K, L-1)); //cout << "hi\n";
+            return 0;
+        }
 
-        if(K<n && !vis[K+1][L] && (s[K+1][L]=='.' || s[K+1][L]=='G')) Q.push(MP(K+1, L));
-        else if(K<n && !vis[K+1][L] && s[K+1][L]=='B') return 0;
+        //if(K<n && vis[K+1][L]) continue;
+        if(K<n && !vis[K+1][L] && (s[K+1][L]=='.' || s[K+1][L]=='G')){
+            //if(K+1==n && L==m) return 1;
+            //vis[K+1][L] = 1;
+            Q.push(MP(K+1, L));
+        }
+        else if(K<n && !vis[K+1][L] && s[K+1][L]=='B'){
+            //if(K+1==n && L==m) return 1;
+            //Q.push(MP(K+1, L));
+            return 0;
+        }
 
-        if(L<m && !vis[K][L+1] && (s[K][L+1]=='.' || s[K][L+1]=='G')) Q.push(MP(K, L+1));
-        else if(L<m && !vis[K][L+1] && s[K][L+1]=='B') return 0;
+        //if(L<m && vis[K][L+1]) continue;
+        if(L<m && !vis[K][L+1] && (s[K][L+1]=='.' || s[K][L+1]=='G')){
+            //if(K==n && L+1==m) return 1;
+            //vis[K][L+1] = 1;
+            Q.push(MP(K, L+1));
+        }
+        else if(L<m && !vis[K][L+1] && s[K][L+1]=='B'){
+            //if(K==n && L+1==m) return 1;
+            //Q.push(MP(K, L+1));
+            return 0;
+        }
 
-        if(K>0 && !vis[K-1][L] && (s[K-1][L]=='.' || s[K-1][L]=='G')) Q.push(MP(K-1, L));
-        else if(K>0 && !vis[K-1][L] && s[K-1][L]=='B') return 0;
+        //if(K>0 && vis[K-1][L]) continue;
+        if(K>0 && !vis[K-1][L] && (s[K-1][L]=='.' || s[K-1][L]=='G')){
+            //if(K-1==n && L==m) return 1;
+            //vis[K-1][L] = 1;
+            Q.push(MP(K-1, L));
+        }
+        else if(K>0 && !vis[K-1][L] && s[K-1][L]=='B'){
+            //if(K-1==n && L==m) return 1;
+            //Q.push(MP(K-1, L));
+            return 0;
+        }
     }
 
     return 1;
@@ -92,9 +167,14 @@ void task()
     cin >> t;
     while(t--){
         cin >> n >> m;
+        //memset(vis, 0, 50);
         for(int K=0; K<n; K++){
             for(int L=0; L<m; L++) vis[K][L] = 0;
         }
+        /*for(int K=0; K<n; K++){
+            for(int L=0; L<n; L++) cout << vis[K][L] << ' ';
+            cout << "\n";
+        }*/
 
         for(int K=0; K<n; K++) cin >> s[K];
 
@@ -109,6 +189,7 @@ void task()
 
         int g = 0;
         bool f = 0;
+        //for(int K=0; K<n; K++) cout << s[K] << "\n";
         for(int K=0; K<n; K++){
             for(int L=0; L<m; L++){
                 if(s[K][L]=='B'){
@@ -136,6 +217,7 @@ void task()
                         break;
                     }
                 }
+                //else if(s[K][L]=='G') vis[K][L] = 1;
             }
             if(f) break;
         }
@@ -150,7 +232,7 @@ void task()
             cout << "No\n";
             continue;
         }
-        else{
+        else{//cout << "hi\n";
             for(int K=0; K<n; K++){
                 for(int L=0; L<m; L++){
                     if(s[K][L]=='G' && !vis[K][L]){
@@ -163,6 +245,28 @@ void task()
             }
         }
         if(!f) cout << "Yes\n";
+
+
+        /*bool x = 1, y = 0;
+        for(int K=0; K<n; K++){
+            for(int L=0; L<m; L++){
+                if(s[K][L]=='B'){
+                    y = bfs(MP(K,L), n-1, m-1);
+                    //cout << t << ' ' << K << ' ' << L << ' ' << y << "\n";
+                }
+                if(s[K][L]=='G'){
+                    x = bfs(MP(K,L), n-1, m-1);
+                    //cout << t << ' ' << K << ' ' << L << ' ' << x << "\n";
+                }
+                if(y || !x){
+                    break;
+                }
+            }
+            if(y || !x) break;
+        }
+
+        if(y || !x) cout << "No\n";
+        else cout << "Yes\n";*/
     }
 }
 
@@ -178,4 +282,3 @@ int main()
 
     return 0;
 }
-
