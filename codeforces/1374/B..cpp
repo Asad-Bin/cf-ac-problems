7c1254c17d1ace85b32c0c49e2d4da1359ac02b1
Asad <mdasad.binsaber@gmail.com>
2020-06-29 08:51:00 +0600

    Add solution for problem `B - Multiply by 2, divide by 6` Link: https://codeforces.com/contest/1374/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1374/submission/85417516

---
+++ codeforces/1374/B..cpp
@@ -0,0 +1,112 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 29 06 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+int gcd (int a, int b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return (a*b/gcd(a, b));
+}
+ll comb(ll a, ll b)
+{
+    ll x = max(a-b, b), ans=1;
+
+    for(ll K=a, L=1; K>=x+1; K--, L++){
+        ans *= K;
+        ans /= L;
+    }
+
+    return ans;
+}
+
+
+void bruce()
+{
+    cout << "\nExpected answer:\n";
+    cout << "\nAnswer recieved:\n";
+}
+
+void task()
+{
+    //code here...
+
+    int t, n;
+
+    cin >> t;
+    while(t--){
+        cin >> n;
+
+        if(n==1) {
+            cout << 0 << "\n";
+            continue;
+        }
+
+        int cnt2=0, cnt3=0, ans=0;
+
+        while(1){
+            if(n%2==0){
+                cnt2++;
+                n /= 2;
+            }
+            if(n%3==0){
+                cnt3++;
+                n /= 3;
+            }
+
+            if(n%2!=0 && n%3!=0) break;
+        }
+
+        if(n>1 || cnt3<cnt2){
+            cout << "-1\n";
+            continue;
+        }
+
+        cout << cnt3+cnt3-cnt2 << "\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
