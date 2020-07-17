d0f260ec1bc51c0168bea734ed87197e69be3dcc
Asad <mdasad.binsaber@gmail.com>
2020-07-17 20:58:00 +0600

    Add solution for problem `A - Three Pairwise Maximums` Link: https://codeforces.com/contest/1385/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1385/submission/87101247

---
+++ codeforces/1385/A..cpp
@@ -0,0 +1,102 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return (a/gcd(a, b)*b);
+}
+ll comb(ll a, ll b)
+{
+    ll x = max(a-b, b), ans=1;
+
+    for(ll K=a, L=1; K>=x+1; K--, L++){
+        ans *= K;
+        ans /= L;
+    }
+
+    return ans;
+}
+
+
+void task()
+{
+    //code here...
+    
+    int t, a, b, c, x, y, z;
+    
+    cin >> t;
+    while(t--){
+		cin >> x >> y >> z;
+		
+		if(x==y && x>z){
+			a = x;
+			b = c = z;
+			cout << "YES\n" << a << ' ' << b <<  ' ' << c << "\n";
+		}
+		else if(y==z && y>x){
+			c = y;
+			a = b = x;
+			cout << "YES\n" << a << ' ' << b <<  ' ' << c << "\n";
+		}
+		else if(x==z && x>y){
+			b = x;
+			a = c = y;
+			cout << "YES\n" << a << ' ' << b <<  ' ' << c << "\n";
+		}
+		else if(x==y && y==z){
+			cout << "YES\n" << x << ' ' << y <<  ' ' << z << "\n";
+		}
+		else{
+			cout << "NO\n";
+		}
+	}
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
