8a9024d5dd96c74af0bc5d617c1abf9a80e09410
Asad <mdasad.binsaber@gmail.com>
2020-07-17 21:03:00 +0600

    Add solution for problem `B - Restore the Permutation by Merger` Link: https://codeforces.com/contest/1385/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1385/submission/87106683

---
+++ codeforces/1385/B..cpp
@@ -0,0 +1,94 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return (a/gcd(a, b)*b);
+}
+ll comb(ll a, ll b)
+{
+    ll x = max(a-b, b), ans=1;
+
+    for(ll K=a, L=1; K>=x+1; K--, L++){
+        ans *= K;
+        ans /= L;
+    }
+
+    return ans;
+}
+
+
+void task()
+{
+    //code here...
+    
+    int t, n;
+    
+    cin >> t;
+    while(t--){
+		cin >> n;
+		int ara[2*n];
+		for(int K=0; K<2*n; K++) cin >> ara[K];
+		
+		vi v;
+		vector<bool> mark(n+1, 0);
+		for(int K=0; K<2*n; K++){
+			if(!mark[ara[K]]){
+				v.PB(ara[K]);
+				mark[ara[K]] = 1;
+			}
+		}
+		
+		for(int K=0; K<(int)v.size(); K++) cout << v[K] << ' ';
+		cout << "\n";
+	}
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
