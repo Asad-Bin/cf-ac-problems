2b7f1dde36d9b4f3b422aef0b8f8ba5c59ed788a
Asad <mdasad.binsaber@gmail.com>
2020-07-21 17:12:00 +0600

    Add solution for problem `C - Vasya and Petya's Game` Link: https://codeforces.com/contest/577/problem/C Tags: implementation, number theory, *1500 Ref: https://codeforces.com/contest/577/submission/87507039

---
+++ codeforces/577/C..cpp
@@ -0,0 +1,142 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+ int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+ int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+
+// structs...
+struct hash_pair { 
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const
+    { 
+        auto hash1 = hash<T1>{}(p.first); 
+        auto hash2 = hash<T2>{}(p.second); 
+        return hash1 ^ hash2; 
+    } 
+}; 
+
+// functions...
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return (a/gcd(a, b)*b);
+}
+ll comb(ll a, ll b)
+{
+    ll x = max(a-b, b), ans=1;
+
+    for(ll K=a, L=1; K>=x+1; K--, L++){
+        ans *= K;
+        ans /= L;
+    }
+
+    return ans;
+}
+
+
+vector<int> primes;
+vector<bool> mark(1002);
+void sieve(int n)
+{
+    int i, j, limit = sqrt(n*1.) + 2;
+
+    mark[1] = 1;
+
+    for(i = 4; i<=n; i+=2) mark[i] = 1;
+
+    primes.push_back(2);
+
+    for(i = 3; i <= n; i += 2)
+    {
+        if(!mark[i])
+        {
+            primes.push_back(i);
+
+            if(i<=limit)
+            {
+                for(j = i*i; j <= n; j += i*2)
+                {
+                    mark[j] = 1;
+                }
+            }
+        }
+    }
+}
+void task()
+{
+    //code here...
+    
+    int n;
+    
+    cin >> n;
+    
+    sieve(n);
+    vi ara;
+    
+    for(int K=2; K<=n; K++){
+		if(mark[K]) continue;
+		
+		int i = K;
+		while(i<=n){
+			ara.PB(i);
+			i *= K;
+		}
+	}
+	
+	cout << (int)ara.size() << "\n";
+	for(int K=0; K<(int)ara.size(); K++) cout << ara[K] << ' ';
+	cout << "\n";
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
