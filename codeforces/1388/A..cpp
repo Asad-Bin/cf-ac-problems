07b91228fda6cfc47b5ac34fc5e47a2cab6db4a8
Asad <mdasad.binsaber@gmail.com>
2020-07-30 20:53:00 +0600

    Add solution for problem `A - Captain Flint and Crew Recruitment` Link: https://codeforces.com/contest/1388/problem/A Tags: brute force, greedy, math, number theory, *800 Ref: https://codeforces.com/contest/1388/submission/88468162

---
+++ codeforces/1388/A..cpp
@@ -0,0 +1,126 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+const int MOD = 1e9+7; // 998244353;
+const int MX = 2e5+5;
+const int INF = 1e9+5;
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+
+// structs...
+struct hash_pair { 
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const
+    { 
+        auto hash1 = hash<T1>{}(p.first); 
+        auto hash2 = hash<T2>{}(p.second); 
+        return hash1 ^ hash2; 
+    } 
+}; 
+
+// functions...
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return (a/gcd(a, b)*b);
+}
+ll comb(ll a, ll b)
+{
+    ll x = max(a-b, b), ans=1;
+
+    for(ll K=a, L=1; K>=x+1; K--, L++){
+        ans *= K;
+        ans /= L;
+    }
+
+    return ans;
+}
+
+
+void task()
+{
+	// Code here...
+	
+	int t, n;
+	
+	cin >> t;
+	while(t--){
+		cin >> n;
+		
+		int a = 6, b = 10, c = 14;
+		
+		if(a+b+c>=n){
+			cout << "NO\n";
+			continue;
+		}
+		
+		if(n-(a+b+c)==a){
+			c = 15;
+		}
+		if(n-(a+b+c)==b){
+			c = 15;
+		}
+		if(n-(a+b+c)==c){
+			c = 15;
+		}
+		
+		if(a>0 && b>0 && c>0 && (n-(a+b+c))>0) cout << "YES\n" << a << ' ' << b << ' ' << c << ' ' << n-(a+b+c) << "\n";
+		else cout << "NO\n";
+	}
+}
+
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+	
+	task();
+
+    return 0;
+}
+
