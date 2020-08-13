1585d83955800bc6b23fb85ae06e37ed240311e9
Asad <mdasad.binsaber@gmail.com>
2020-08-13 19:10:00 +0600

    Add solution for problem `C - Boboniu and Bit Operations` Link: https://codeforces.com/contest/1395/problem/C Tags: bitmasks, brute force, dp, greedy, *1600 Ref: https://codeforces.com/contest/1395/submission/89796641

---
+++ codeforces/1395/C..cpp
@@ -0,0 +1,129 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+const int Mod = 1e9+7; // 998244353;
+const int MX = 2e5+5;
+const int INF = 1e5+5;
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+
+// structs...
+struct hash_pair { 
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const
+    { 
+        auto hash1 = hash<T1>{}(p.first); 
+        auto hash2 = hash<T2>{}(p.second); 
+        return hash1 ^ hash2; 
+    } 
+}; 
+
+// functions...
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+ll lcm(ll a, ll b)
+{
+    return (a/gcd(a, b)*b);
+}
+ll comb(ll a, ll b, ll M)
+{
+    ll x = max(a-b, b), ans=1;
+
+    for(ll K=a, L=1; K>=x+1; K--, L++){
+        ans *= K;
+        ans /= L;
+    }
+
+    return ans % M;
+}
+
+
+int n, m;
+int a[512], b[512];
+bool chk(int x)
+{
+	for(int K=0; K<n; K++){
+		bool ok = 0;
+		for(int L=0; L<m; L++){
+			if(((a[K]&b[L])|x)==x){
+				ok = 1;
+				break;
+			}
+		}
+		if(!ok) return 0;
+	}
+	
+	return 1;
+}
+void task()
+{
+    //code here...
+    
+    
+    cin >> n >> m;
+    
+    for(int K=0; K<n; K++) cin >> a[K];
+    for(int K=0; K<m; K++) cin >> b[K];
+    
+    for(int K=0; K<=512; K++){
+		if(chk(K)){
+			cout << K << "\n";
+			return;
+		}
+	}
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    return 0;
+}
+
+
