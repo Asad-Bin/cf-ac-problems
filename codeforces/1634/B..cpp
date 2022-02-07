1bc939ea132f524dcec3d3c939ec68f8487fa89f
Asad <mdasad.binsaber@gmail.com>
2022-02-07 08:49:00 +0600

    Add solution for problem `B - Fortune Telling` Link: https://codeforces.com/contest/1634/problem/B Tags: bitmasks, math, *1400 Ref: https://codeforces.com/contest/1634/submission/145491808

--- codeforces/1634/B..cpp
+++ codeforces/1634/B..cpp
@@ -56,7 +56,6 @@ int main()
         //freopen("out.txt","w",stdout);
     #endif
 	
-	
     ll t, n, x, y, num;
     
     cin >> t;
@@ -78,3 +77,49 @@ int main()
     #endif
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
