e3ee8e348cb8aad675e3a563aa501f53d27a98b6
Asad <mdasad.binsaber@gmail.com>
2023-06-06 22:35:00 +0600

    Add solution for problem `B - Binary Cafe` Link: https://codeforces.com/contest/1840/problem/B Tags: bitmasks, combinatorics, math, *1100 Ref: https://codeforces.com/contest/1840/submission/208802248

---
+++ codeforces/1840/B..cpp
@@ -0,0 +1,125 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#ifndef ONLINE_JUDGE
+#define dbg_out cout
+#define debug(...) dbg_out << "DBG )> "; __f(#__VA_ARGS__, __VA_ARGS__);
+template<typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2> pr) { out << "{ " << pr.first << ", " << pr.second << " }"; return out; }
+template<typename T1> ostream& operator<<(ostream& out, vector<T1> vec) { out << "{ "; for (auto &x: vec) out << x << ", "; out << "}"; return out; }
+template<typename T1, size_t size> ostream& operator<<(ostream& out, array<T1, size> arr) { out << "{ "; for (auto &x: arr) out << x << ", "; out << "}"; return out; }
+template<typename T1, typename T2> ostream& operator<<(ostream& out, map<T1, T2> mp) { out << "{ ";for (auto &x: mp) out << x.first << ": " << x.second <<  ", "; out << "}"; return out; }
+template <typename Arg1> void __f(const char* name, Arg1&& arg1) { while (isspace(name[0])) name++; (isalpha(name[0]) || name[0] == '_') ? dbg_out << name << ": " << arg1 << "\n" : dbg_out << arg1 << "\n"; dbg_out.flush();}
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) { const char *comma = strchr(names + 1, ','); while (isspace(names[0])) names++; (isalpha(names[0]) || names[0] == '_') ? dbg_out.write(names, comma - names) << ": " << arg1 << " | " : dbg_out << arg1 << " | "; __f(comma + 1, args...);}
+#else
+#define debug(...)
+#endif
+
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K; ans /= L;} return ans;}
+ll bigmod(ll a,ll b,ll mod){ if(b==0){ return 1;} ll tm=bigmod(a,b/2,mod); tm=(tm*tm)%mod; if(b%2==1) tm=(tm*a)%mod; return tm;}
+ll egcd(ll a,ll b,ll &x,ll &y){ if(a==0){ x=0; y=1; return b;} ll x1,y1; ll d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
+ll modpow(ll a,ll p,ll mod) {ll ans=1;while(p){if(p%2)ans=(ans*a)%mod;a=(a*a)%mod;p/=2;} return ans;}
+ll inverse_mod(ll n,ll mod) {return modpow(n,mod-2,mod);}
+
+
+//struct hash_pair {
+    //template <class T1, class T2>
+    //size_t operator()(const pair<T1, T2>& p) const
+    //{
+        //auto hash1 = hash<T1>{}(p.first);
+        //auto hash2 = hash<T2>{}(p.second);
+ 
+        //if (hash1 != hash2) {
+            //return hash1 ^ hash2;             
+        //}
+         
+        //// If hash1 == hash2, their XOR is zero.
+          //return hash1;
+    //}
+//};
+
+int n, k;
+//unordered_map<pair<int, int>, int, hash_pair> dp;
+int calc(int at, int sum)
+{
+	if(sum == 0) return 1;
+	//if((1ll<<at) > n) return 1;
+	if(at == 0) return 2;
+	
+	//if(dp.find({at, sum}) != dp.end()) return dp[{at, sum}];
+	
+	
+		int i = 0;
+		while(i <= at && (1<<i) <= sum) i++;
+		i--;
+		
+		return calc(i-1, sum-(1<<i))+(1<<i);
+	
+	//int ans = 0;
+	//ans += calc(at-1, sum);
+	//if((1ll<<at) <= sum) ans += calc(at-1, sum-(1ll<<at));
+	
+	//cout << at << ' ' << sum << ' ' << ans << "\n";
+	//return ans;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int t; cin >> t;
+	while(t--){
+		cin >> n >> k;
+		
+		//dp.clear();
+		int i = 0;
+		while((1<<i) <= n) i++;
+		i--;
+		
+		cout << (i <= k-1 ? calc(i-1, n-(1<<i))+(1<<i) : calc(k-1, n)) << "\n";
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
