8f2af387fd382e5c8c37ad17d55a903a980cf282
Asad <mdasad.binsaber@gmail.com>
2024-05-31 17:19:00 +0600

    Add solution for problem `C - Turtle and an Incomplete Sequence` Link: https://codeforces.com/contest/1981/problem/C Tags: bitmasks, brute force, constructive algorithms, greedy, implementation, math, *1800 Ref: https://codeforces.com/contest/1981/submission/263478075

---
+++ codeforces/1981/C..cpp
@@ -0,0 +1,249 @@
+
+// . . . Bismillahir Rahmanir Rahim . . .
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#ifndef ONLINE_JUDGE
+#define dbg_out cout
+#define debug(...) dbg_out << "DBG )> "; __f(#__VA_ARGS__, __VA_ARGS__);
+template<typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2> pr) { out << "{ " << pr.first << ", " << pr.second << " }"; return out; }
+template<typename T1> ostream& operator<<(ostream& out, vector<T1> vec) { out << "{ "; for (auto &x: vec) out << x << ", "; out << "}"; return out; }
+template<typename T1, size_t size> ostream& operator<<(ostream& out, array<T1, size> arr) { out << "{ "; for (auto &x: arr) out << x << ", "; out << "}"; return out; }
+template<typename T1, typename T2> ostream& operator<<(ostream& out, map<T1, T2> mp) { out << "{ ";for (auto &x: mp) out << x.first << ": " << x.second <<  ", "; out << "}"; return out; }
+template <typename Arg1> void __f(const char* name, Arg1&& arg1) { while (isspace(name[0])) name++; (isalpha(name[0]) || name[0] == '_') ? dbg_out << name << ": " << arg1 << "\n" : dbg_out << arg1 << "\n"; dbg_out.flush();}
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) { const char *comma = strchr(names + 1, ','); while (isspace(names[0])) names++; (isalpha(names[0]) || names[0] == '_') ? dbg_out.write(names, comma - names) << ": " << arg1 << " | " : dbg_out << arg1 << " | "; __f(comma + 1, args...);}
+#else
+#define debug(...)
+#endif
+
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K; ans /= L;} return ans;}
+ll bigmod(ll a,ll b,ll mod){ 
+    b %= (mod-1);
+    a %= mod;
+
+    if(b==0){
+     return 1;
+    } 
+    ll tm=bigmod(a,b/2,mod); 
+    tm=(tm*tm)%mod; 
+    if(b%2==1) tm=(tm*a)%mod; 
+    return tm;
+}
+ll egcd(ll a,ll b,ll &x,ll &y){ if(a==0){ x=0; y=1; return b;} ll x1,y1; ll d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
+ll modpow(ll a,ll p,ll mod) {ll ans=1;while(p){if(p%2)ans=(ans*a)%mod;a=(a*a)%mod;p/=2;} return ans;}
+ll inverse_mod(ll n,ll mod) {return modpow(n,mod-2,mod);}
+
+
+
+const ll MXI = 1e9;
+int bit_cnt(ll x)
+{
+	int ans = 0;
+	while(x) {
+		x /= 2;
+		ans++;
+	}
+	return ans;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1; 
+    cin >> t;
+    for(int cs = 1; cs <= t; cs++){
+         //cout << "Case " << cs << ": ";
+        
+        ll n; cin >> n;
+        ll ara[n];
+        for(int K = 0; K < n; K++) cin >> ara[K];
+        
+        for(int K = 0; K < n; K++){
+			if(ara[K] != -1){
+				for(int L = K+1; L < n; L++){
+					if(ara[L] != -1){
+						ll i = K+1, j = L-1, l = ara[K], r = ara[L];
+						
+						while(i <= j && bit_cnt(l) > bit_cnt(r)){
+							if(l == 1){
+								l *= 2;
+								ara[i++] = l;
+								continue;
+							}
+							ara[i++] = l/2;
+							l /= 2;
+						}
+						while(i <= j && bit_cnt(l) < bit_cnt(r)){
+							if(r == 1){
+								r *= 2;
+								ara[j--] = r;
+								continue;
+							}
+							ara[j--] = r/2;
+							r /= 2;
+						}
+						
+						//cout << "lr " << l << ' ' << r << "\n";
+						
+						while(i <= j && l != r){
+							if(l > 1){
+								ara[i++] = l/2;
+								l /= 2;
+							}
+							if(r > 1){
+								ara[j--] = r/2;
+								r /= 2;
+							}
+						}
+						
+						bool ff = 1;
+						while(i <= j && l == r){
+							if(ff){
+								ara[i++] = l*2;
+								ara[j--] = r*2;
+								l *= 2;
+								r *= 2;
+							}
+							else{
+								ara[i++] = l/2;
+								ara[j--] = r/2;
+								l /= 2;
+								r /= 2;
+							}
+							
+							ff ^= 1;
+						}
+						
+						K = L-1;
+						break;
+					}
+				}
+			}
+		}
+		
+		for(int K = 0; K < n; K++){
+			if(ara[K] != -1){
+				ll i = ara[K]*2;
+				bool f = 1;
+				
+				for(int L = K-1; L >= 0; L--){
+					if(f){
+						ara[L] = i;
+						
+						i /= 2;
+						f = 0;
+					}
+					else{
+						ara[L] = i;
+						
+						i *= 2;
+						f = 1;
+					}
+				}
+				
+				break;
+			}
+			if(K == n-1){
+				ll i = 2;
+				bool f = 1;
+				
+				for(int L = K; L >= 0; L--){
+					if(f){
+						ara[L] = i;
+						
+						i /= 2;
+						f = 0;
+					}
+					else{
+						ara[L] = i;
+						
+						i *= 2;
+						f = 1;
+					}
+				}
+				
+				break;
+			}
+		}
+		for(int K = n-1; K >= 0; K--){
+			if(ara[K] != -1){
+				ll i = ara[K]*2;
+				bool f = 1;
+				
+				for(int L = K+1; L < n; L++){
+					if(f){
+						ara[L] = i;
+						
+						i /= 2;
+						f = 0;
+					}
+					else{
+						ara[L] = i;
+						
+						i *= 2;
+						f = 1;
+					}
+				}
+				
+				break;
+			}
+		}
+		
+		bool ok = 1;
+		for(int K = 0; K < n; K++){
+			ll cnt = 0;
+			if(K ==n-1 || (ara[K]*2 == ara[K+1] || ara[K]*2+1 == ara[K+1] || ara[K] == ara[K+1]*2 || ara[K] == ara[K+1]*2+1)) cnt++;
+			if(K == 0 || (ara[K]*2 == ara[K-1] || ara[K]*2+1 == ara[K-1] || ara[K] == ara[K-1]*2 || ara[K] == ara[K-1]*2+1)) cnt++;
+			
+			if(cnt == 2) {}
+			else ok = 0;
+		}
+		
+		if(ok){}
+		else{
+			cout << -1 << "\n";
+			continue;
+		}
+		
+		for(int K = 0; K < n; K++) cout << ara[K] << ' ';
+		cout << "\n";
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
